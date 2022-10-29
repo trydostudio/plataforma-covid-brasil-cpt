42d851a07b7494cc78b06521127c467663c7a83e
André Hellmann <andrehellmann@MacBook-Pro-de-Andre.local>
2022-10-29 13:13:25 -0300

    chore: version 0.2.0 add filter plugin to api

--- plataforma-covid-brasil-cpt.php
+++ plataforma-covid-brasil-cpt.php
@@ -6,7 +6,7 @@
  * Description:       Wordpress Custom Post Types to Plataforma Covid Brasil - OPAS/OMS.
  * Requires at least: 5.9
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.2.0
  * Author:            http://trydo.com.br
  * License:           Unlisenced
  * Text Domain:       pcbcpt
@@ -16,3 +16,4 @@
 
 require_once plugin_dir_path(__FILE__) . 'inc/registerCustomPost.php';
 require_once plugin_dir_path(__FILE__) . 'inc/routes/searchRoute.php';
+require_once plugin_dir_path(__FILE__) . 'inc/filterApi.php';
